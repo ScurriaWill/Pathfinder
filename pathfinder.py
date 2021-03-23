ef261481838babeaa330b0c50d7dc213bf498562
21scurria <willscurria@gmail.com>
2021-03-23 13:39:43 -0400

    added normalize array function

--- pathfinder.py
+++ pathfinder.py
@@ -1,6 +1,23 @@
+import sys
+
+
 def print_two_d_array(item):
     for num in range(len(item)):
         print(item[num])
+
+
+def normalize_array(array):
+    min_num = sys.maxsize
+    max_num = -sys.maxsize
+    for line in array:
+        for num in line:
+            min_num = min(min_num, num)
+            max_num = max(max_num, num)
+    max_num = max_num - min_num
+    for line_num in range(len(array)):
+        for i in range(len(array[line_num])):
+            array[line_num][i] = (100 * (array[line_num][i] - min_num)) / max_num
+    return array
 
 
 def readfile(file_name):
@@ -17,7 +34,9 @@ def readfile(file_name):
 
 
 def main():
-    array = readfile("testMountains.dat")
+    array = readfile("Colorado_480x480.dat")
+    print_two_d_array(array)
+    array = normalize_array(array)
     print_two_d_array(array)
 
 
