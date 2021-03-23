d6d24ce4deb6b8ddcdc9f07617df8ab5b50e0ba5
21scurria <willscurria@gmail.com>
2021-03-23 13:22:54 -0400

    adding read file

--- pathfinder.py
+++ pathfinder.py
@@ -1,1 +1,25 @@
-print("hello world")
+def print_two_d_array(item):
+    for num in range(len(item)):
+        print(item[num])
+
+
+def readfile(file_name):
+    file = open(file_name, "r")
+    array = []
+    for x in file:
+        line = x.split()
+        int_line = []
+        for num in range(len(line)):
+            item = int(line[num])
+            int_line.append(item)
+        array.append(int_line)
+    return array
+
+
+def main():
+    array = readfile("testMountains.dat")
+    print_two_d_array(array)
+
+
+if __name__ == "__main__":
+    main()
